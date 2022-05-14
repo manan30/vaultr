30ed30cccd954617f53ef7cb3bb8fc3bab5c61c8
Manan Joshi <mananjoshi1995@gmail.com>
2022-05-13 22:03:49 -0500

    chore: organizing service app module

--- apps/vaultr-service/src/main.ts
+++ apps/vaultr-service/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/App/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(1741);
 }
+
 bootstrap();
