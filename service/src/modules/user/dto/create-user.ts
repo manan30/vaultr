6419a791c9303809d0540bf7dad943abe0f9ba25
Manan Joshi <mananjoshi1995@gmail.com>
2023-03-17 22:38:12 -0500

    feat: adding create user dto

---
+++ service/src/modules/user/dto/create-user.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(8)
+  @IsNotEmpty()
+  password: string;
+}
