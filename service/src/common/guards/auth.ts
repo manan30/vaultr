2f0fc47a1896edc25ac28b395043508ed4f778f3
Manan Joshi <mananjoshi1995@gmail.com>
2023-03-21 22:41:26 -0500

    feat: adding auth guard

---
+++ service/src/common/guards/auth.ts
@@ -0,0 +1,24 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { Request } from 'express';
+import { Session } from 'src/types/session';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const req: Request = context.switchToHttp().getRequest();
+    const { session } = req;
+    const { cookie, user } = session as unknown as Session;
+
+    if (!session || !cookie || !cookie.expires || !user) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+}
